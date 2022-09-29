cc78384e0c5fa7f82ddcde9f74374dbcefc3b4b6
BinhNguyen <ntbinh200394@gmail.com>
2022-09-29 16:47:48 +0700

    EXE create form Login/Logout

--- 3-StateAndLC/my-app/src/App.js
+++ 3-StateAndLC/my-app/src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
+import Authen from './components/Authen';
 import Calculator from './components/Calculator';
 import ConditionalRendering from './components/ConditionalRendering';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       {/* <ConditionalRendering /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      <Login />
     </div>
   );
 }
